5b36ca4f6ca8d9c4843564be66eee08641cc884f
jp <jjorissen52@gmail.com>
2017-11-03 18:53:35 -0500

    added some things to make it look good

--- solid/admin.py
+++ solid/admin.py
@@ -1,3 +1,12 @@
+from copy import deepcopy
 from django.contrib import admin
+from mezzanine.galleries.admin import GalleryAdmin
+from mezzanine.galleries.models import Gallery
 
-# Register your models here.
+blog_fieldsets = deepcopy(GalleryAdmin.fieldsets)
+
+class MyGalleryAdmin(GalleryAdmin):
+    fieldsets = blog_fieldsets
+
+admin.site.unregister(Gallery)
+admin.site.register(Gallery, MyGalleryAdmin)
